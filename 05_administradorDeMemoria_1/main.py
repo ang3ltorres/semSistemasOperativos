8e742ceb55fe4e0da00987184de9fdd4a71c99f4
Angel <angel.storres@alumnos.udg.mx>
2023-09-23 17:51:39 -0600

    gui fix

--- 05_administradorDeMemoria_1/main.py
+++ 05_administradorDeMemoria_1/main.py
@@ -20,33 +20,36 @@ class MemoryPainter(QWidget):
 	def paintEvent(self, event):
 		painter = QPainter(self)
 
-		# Pintar
+		# Calculos
 		width = int(self.width() * 0.6)
 		totalHeight = sum(memory_block.totalSize for memory_block in self.manager.memory)
 		factor = (self.height() * 0.9) / totalHeight
+
+		# Pens
+		pen_rect = QPen(QColorConstants.Black, 2, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap)
+		pen_text = QPen(QColorConstants.Black)
 
 		# Colores
 		currentColor = True
 		color1 = QColor(204, 204, 255)
 		color2 = QColor(204, 255, 255)
 		
-		
 		y = 10
 		for i in range(len(self.manager.memory)):
 			
-			painter.setPen(QPen(QColorConstants.Black, 4, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap))
+			painter.setPen(pen_rect)
 			x = int((self.width() / 2) - (width / 2))
 
 			# Dibujar los bloques de memoria
 			height = int(self.manager.memory[i].totalSize * factor)
 			painter.fillRect(x, y, width, height, QColorConstants.Black)
 
-			# Numero del bloque
-			painter.setPen(QPen(QColorConstants.Black))
+			# Numero y size del bloque
+			painter.setPen(pen_text)
 			painter.setFont(self.font_big)
 			painter.drawText(x - 50, y + 18, f"[{i}]")
-			painter.drawText(x + width, y + 18, f"[{self.manager.memory[i].totalSize}]")
-			painter.setPen(QPen(QColorConstants.Black, 4, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap))
+			painter.drawText(x + width, y + 18, f"[{self.manager.memory[i].totalSize}kb]")
+			painter.setPen(pen_rect)
 
 			# Dibujar los procesos (espacio utilizado)
 			process_y = y
@@ -56,17 +59,14 @@ class MemoryPainter(QWidget):
 				currentColor = not currentColor
 
 				# Dibujar datos del proceso
-				painter.setPen(QPen(QColorConstants.Black))
+				painter.setPen(pen_text)
 				painter.setFont(self.font_normal)
 				painter.drawText(x + 8, process_y + 14, f"{self.manager.memory[i].process[j].name}:  {self.manager.memory[i].process[j].size}kb")
-				painter.setPen(QPen(QColorConstants.Black, 4, Qt.PenStyle.SolidLine, Qt.PenCapStyle.RoundCap))
 
 				process_y += process_height
 
+			painter.setPen(pen_rect)
 			painter.drawRect(x, y, width, height)
-
-
-
 			y += height + 8
 
 class MainWindow(QMainWindow):
@@ -82,14 +82,11 @@ class MainWindow(QMainWindow):
 		self.layout = QVBoxLayout(self.w)
 
 		# Memory manager
-		process = process_from_file('archivos.txt')
 		self.manager = Manager([1000, 400, 1800, 700, 900, 1200, 1500])
-		self.manager.algorithm = Algorithm.MEJOR_AJUSTE
-		self.manager.insert_process(process)
-		print(self.manager)
 
 		# Menu file
 		open_action = QAction('Abrir', self)
+		open_action.triggered.connect(self.open_file)
 
 		file_menu = self.menuBar().addMenu('Archivo')
 		file_menu.addAction(open_action)
@@ -98,13 +95,34 @@ class MainWindow(QMainWindow):
 		self.group = QGroupBox("Memoria", self)
 		self.layout_group_form = QVBoxLayout(self.group)
 
+		self.combobox_algorithm = QComboBox(self.group)
+		self.combobox_algorithm.addItem("Primer ajuste")
+		self.combobox_algorithm.addItem("Mejor ajuste")
+		self.combobox_algorithm.addItem("Peor ajuste")
+		self.combobox_algorithm.addItem("Siguiente ajuste")
+		self.combobox_algorithm.activated.connect(self.change_algorithm)
+
 		# Painter
 		self.painter = MemoryPainter(self.manager, self.group)
+
+		# Layout
+		self.layout_group_form.addWidget(self.combobox_algorithm)
 		self.layout_group_form.addWidget(self.painter)
 		
 		# Agregar al layout principal
 		self.layout.addWidget(self.group)
 		self.w.setLayout(self.layout)
+
+	def open_file(self):
+		file_name, _ = QFileDialog.getOpenFileName(self, 'Abrir archivo', '', 'Archivos de Texto (*.txt);;Todos los archivos (*)')
+		if file_name:
+			process = process_from_file('archivos.txt')
+			self.manager.insert_process(process)
+			print(self.manager)
+
+	def change_algorithm(self):
+		self.manager.algorithm = Algorithm(self.combobox_algorithm.currentIndex())
+		print(self.manager.algorithm)
 
 if __name__ == '__main__':
 	import sys
